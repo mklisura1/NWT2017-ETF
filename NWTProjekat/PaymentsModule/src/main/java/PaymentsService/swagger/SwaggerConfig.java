bde4a621ab4987e21ce5fcef2f66ab589cb7cd37
Haris Spahic <spahaa@hotmail.com>
2017-04-05 15:33:35 +0200

    Dodan Swagger UI (http://localhost:1102/swagger-ui.html)

--- NWTProjekat/PaymentsModule/src/main/java/PaymentsService/swagger/SwaggerConfig.java
+++ NWTProjekat/PaymentsModule/src/main/java/PaymentsService/swagger/SwaggerConfig.java
@@ -1,10 +1,12 @@
 package PaymentsService.swagger;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -22,6 +24,19 @@ public class SwaggerConfig extends WebMvcConfigurerAdapter{
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        ApiInfo apiInfo = new ApiInfo(
+                "PaymentsModule REST API",
+                "NWT Projekat",
+                "2017",
+                "Terms of service",
+                "hspahic1@etf.unsa.ba",
+                "License of API",
+                "API license URL");
+        return apiInfo;
     }
 }
