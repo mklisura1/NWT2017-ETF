487d4c7fe9a1d34003475b542019efa5a3976f62
anes.luckin@gmail.com <aluckin1@etf.unsa.ba>
2017-04-07 21:13:43 +0200

    TransactionsModule registrovan kao Eureka client i dodana enkripcija
    - Registrovan Transactions Module kao Eureka client
    - Dodan kljuc u Transactions bootstrap.properties
    - Dodan hash password-a na bazu u transactions.properties

--- NWTProjekat/TransactionsModule/src/main/java/TransactionsService/TransactionsApplication.java
+++ NWTProjekat/TransactionsModule/src/main/java/TransactionsService/TransactionsApplication.java
@@ -1,10 +1,16 @@
 package TransactionsService;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 @PropertySource({"classpath:application.properties"})
 @SpringBootApplication
 @RestController
+@EnableDiscoveryClient
 public class TransactionsApplication{
     @Value("${user.role}")
     private String role;
@@ -30,7 +37,19 @@ public class TransactionsApplication{
             method = RequestMethod.GET,
             produces = MediaType.TEXT_PLAIN_VALUE)
     public String whoami(@PathVariable("username") String username) {
-        return String.format("Hello! You're %s and you'll become a(n) %s...\n", username, role);
+        return String.format("Hello! You're %s and you'll become a(n) %s...", username, role);
     }
 }
 
+@RestController
+class ServiceInstanceRestController {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @RequestMapping("/service-instances/{applicationName}")
+    public List<ServiceInstance> serviceInstancesByApplicationName(
+            @PathVariable String applicationName) {
+        return this.discoveryClient.getInstances(applicationName);
+    }
+}
